efbc877934f1cabbda6a2551c00d8fb7641b517c
Alexander Haberl <alexander.e360.haberl@tuwien.ac.at>
2022-11-04 16:38:59 +0100

    Move get_minimum_oriented_bounding_box function from table_plane_extractor

--- src/v4r_util/util.py
+++ src/v4r_util/util.py
@@ -6,6 +6,8 @@ import rospy
 from vision_msgs.msg import BoundingBox3D, BoundingBox3DArray
 from tf2_sensor_msgs.tf2_sensor_msgs import do_transform_cloud
 from visualization_msgs.msg import Marker, MarkerArray
+import compas.geometry.bbox as compas_bb
+
 
 def o3d_bb_to_ros_bb(o3d_bb):
     '''
@@ -27,8 +29,9 @@ def o3d_bb_to_ros_bb(o3d_bb):
     ros_bb.size.x = o3d_bb.extent[0]
     ros_bb.size.y = o3d_bb.extent[1]
     ros_bb.size.z = o3d_bb.extent[2]
-    
+
     return ros_bb
+
 
 def ros_bb_to_o3d_bb(ros_bb):
     '''
@@ -36,15 +39,17 @@ def ros_bb_to_o3d_bb(ros_bb):
     Input: vision_msgs/BoundingBox3D ros_bb
     Output: open3d.geometry.OrientedBoundingBox o3d_bb
     '''
-    center = np.array([ros_bb.center.position.x, ros_bb.center.position.y, ros_bb.center.position.z])
+    center = np.array(
+        [ros_bb.center.position.x, ros_bb.center.position.y, ros_bb.center.position.z])
     quat = ros_bb.center.orientation
     rot = R.from_quat([quat.x, quat.y, quat.z, quat.w])
     rot_mtx = rot.as_matrix()
     extent = np.array([ros_bb.size.x, ros_bb.size.y, ros_bb.size.z])
 
     o3d_bb = o3d.geometry.OrientedBoundingBox(center, rot_mtx, extent)
-    
+
     return o3d_bb
+
 
 def transformPointCloud(cloud, target_frame, source_frame, tf_buffer):
     ''' 
@@ -53,14 +58,16 @@ def transformPointCloud(cloud, target_frame, source_frame, tf_buffer):
             tf2_ros.Buffer tf_buffer
     Output: sensor_msgs/PointCloud2 transformedCloud
     '''
-    
+
     while not rospy.is_shutdown():
         try:
-            transform = tf_buffer.lookup_transform(target_frame, source_frame, rospy.Time())
+            transform = tf_buffer.lookup_transform(
+                target_frame, source_frame, rospy.Time())
         except:
             continue
         transformedCloud = do_transform_cloud(cloud, transform)
         return transformedCloud
+
 
 def ros_bb_arr_to_rviz_marker_arr(ros_bb_arr, clear_old_markers=True):
     '''
@@ -95,6 +102,7 @@ def ros_bb_arr_to_rviz_marker_arr(ros_bb_arr, clear_old_markers=True):
         marker_arr.markers.append(marker)
     return marker_arr
 
+
 def o3d_bb_list_to_ros_bb_arr(o3d_bb_list, frame_id, stamp):
     '''
     Converts a list of opend3d OrientedBoundingBoxes to a ros BoundingBox3DArray.
@@ -110,4 +118,16 @@ def o3d_bb_list_to_ros_bb_arr(o3d_bb_list, frame_id, stamp):
     for bb_o3d in o3d_bb_list:
         bb_arr_ros.boxes.append(o3d_bb_to_ros_bb(bb_o3d))
     return bb_arr_ros
-    
+
+
+def get_minimum_oriented_bounding_box(o3d_pc):
+    '''
+    Computes the oriented minimum bounding box of a set of points in 3D space.
+    Input: open3d.geometry.PointCloud o3d_pc
+    Output: open3d.geometry.OrientedBoundingBox o3d_bb
+    '''
+    bb_points = np.asarray(
+        compas_bb.oriented_bounding_box_numpy(np.asarray(o3d_pc.points)))
+    o3d_bb = o3d.geometry.OrientedBoundingBox.create_from_points(
+        o3d.utility.Vector3dVector(bb_points))
+    return o3d_bb

bc7ba1fc3ac310f8ab2a81eba386f3ff27717178
Alexander Haberl <alexander.e360.haberl@tuwien.ac.at>
2023-09-14 17:45:17 +0000

    Add list_to_vector3 and bb to bb stamped conversions

--- src/v4r_util/conversions.py
+++ src/v4r_util/conversions.py
@@ -1,5 +1,7 @@
 from copy import deepcopy
+import std_msgs
 import geometry_msgs
+from grasping_pipeline_msgs.msg import BoundingBox3DStamped
 
 def point_to_vector3(point):
     '''
@@ -18,6 +20,13 @@ def vector3_to_list(vec3):
     geometry_msgs/Vector3 to Pythonlist of len() = 3
     '''
     return deepcopy([vec3.x, vec3.y, vec3.z])
+
+def list_to_vector3(list):
+    '''
+    Pythonlist of len() = 3 to geometry_msgs/Vector3
+    '''
+    assert len(list) == 3
+    return deepcopy(geometry_msgs.msg.Vector3(x = list[0], y = list[1], z = list[2]))
 
 def pose_to_transform_stamped(pose, origin_frame, child_frame, stamp):
     '''
@@ -52,3 +61,14 @@ def transform_stamped_to_pose_stamped(transform):
     p.pose.position = vector3_to_point(transform.transform.translation)
     
     return deepcopy(p)
+
+def bounding_box_to_bounding_box_stamped(bounding_box, frame_id, stamp):
+    '''
+    vision_msgs/BoundingBox3D to v4r_msgs/BoundingBox3DStamped
+    '''
+    header = std_msgs.msg.Header(frame_id = frame_id, stamp = stamp)
+    return BoundingBox3DStamped(
+        header = header,
+        center = bounding_box.center,
+        size = bounding_box.size
+        )

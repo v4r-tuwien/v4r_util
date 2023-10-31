9f3feb41e5b9db58ec4b879adc0a3cd8ee1b6bcd
Christian <christianxeder@gmail.com>
2023-10-31 13:21:57 +0100

    Add function that aligns poses and BBs to a reference frame

--- src/v4r_util/util.py
+++ src/v4r_util/util.py
@@ -11,6 +11,7 @@ from geometry_msgs.msg import PoseStamped, Pose
 from geometry_msgs.msg import Point, Quaternion, Vector3
 from grasping_pipeline_msgs.msg import BoundingBox3DStamped
 from std_msgs.msg import Header
+from enum import IntEnum
 
 #TODO should probably split into transform.py and bbstuff.py
 
@@ -276,3 +277,94 @@ def transform_bounding_box(ros_bb, source_frame, target_frame, listener=None):
     transformed_bb.size = ros_bb.size
 
     return transformed_bb
+
+class Axis(IntEnum):
+    X = 0
+    Y = 1
+    Z = 2    
+
+def get_best_aligning_axis(coordinate_axis_vec):
+    x_vec = np.array([1, 0, 0])
+    y_vec = np.array([0, 1, 0])
+    z_vec = np.array([0, 0, 1])
+    
+    x_align = np.abs(np.dot(coordinate_axis_vec, x_vec))
+    y_align = np.abs(np.dot(coordinate_axis_vec, y_vec))
+    z_align = np.abs(np.dot(coordinate_axis_vec, z_vec))
+
+    if x_align >= y_align and x_align >= z_align:
+        return Axis.X, np.dot(coordinate_axis_vec, x_vec) < 0
+    elif y_align >= x_align and y_align >= z_align:
+        return Axis.Y, np.dot(coordinate_axis_vec, y_vec) < 0
+    else:
+        return Axis.Z, np.dot(coordinate_axis_vec, z_vec) < 0
+    
+def align_pose_rotation(pose):
+    # ros pose, not pose stamped
+    center = [pose.position.x, pose.position.y, pose.position.z]
+    transform = np.eye(4)
+    transform[:3, :3] = R.from_quat([pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w]).as_matrix()
+    transform[:3, 3] = np.array(center)
+    
+    pose_x_vec = center - (transform @ np.array([1, 0, 0, 1]))[:3]
+    pose_y_vec = center - (transform @ np.array([0, 1, 0, 1]))[:3]
+    pose_z_vec = center - (transform @ np.array([0, 0, 1, 1]))[:3]
+    new_rot_mat = np.eye(3)
+    axes_aligned = [False, False, False]
+    
+    for coordinate_axis_vec in [pose_x_vec, pose_y_vec, pose_z_vec]:
+        axis, should_invert = get_best_aligning_axis(coordinate_axis_vec)
+        if should_invert:
+            coordinate_axis_vec *= -1
+        new_rot_mat[:, axis] = coordinate_axis_vec
+        axes_aligned[axis] = True
+
+    # check handedness, should theoretically never happen becase reference frame should be right-handed
+    if np.linalg.det(new_rot_mat) < 0:
+        raise ValueError("Rotation matrix has negative determinant")
+        
+    if not axes_aligned[Axis.X] and not axes_aligned[Axis.Y] and not axes_aligned[Axis.Z]:
+        raise ValueError("Could not find consistent alignment")
+    quat = R.from_matrix(new_rot_mat).as_quat()
+    ret_pose = copy.deepcopy(pose)
+    ret_pose.orientation = Quaternion(x=quat[0], y=quat[1], z=quat[2], w=quat[3])
+    return ret_pose
+
+def align_bounding_box_rotation(bb):
+    '''
+    Aligns bounding box rotation to coordinate axes. This means that the BB x-axis will be the one that aligns the best 
+    with the x-axis of the coordinate frame, and so on.
+    Input: o3d/OrientedBoundingBox bb
+    Output: o3d/OrientedBoundingBox bb
+    '''
+    transform = np.eye(4)
+    transform[:3, :3] = bb.R
+    transform[:3, 3] = bb.center
+    
+    bb_x_vec = bb.center - (transform @ np.array([1, 0, 0, 1]))[:3]
+    bb_y_vec = bb.center - (transform @ np.array([0, 1, 0, 1]))[:3]
+    bb_z_vec = bb.center - (transform @ np.array([0, 0, 1, 1]))[:3]
+    new_rot_mat = np.eye(3)
+    new_extent = np.zeros(3)
+    axes_aligned = [False, False, False]
+    
+    for coordinate_axis_vec, extent in zip([bb_x_vec, bb_y_vec, bb_z_vec], [bb.extent[0], bb.extent[1], bb.extent[2]]):
+        axis, should_invert = get_best_aligning_axis(coordinate_axis_vec)
+        if should_invert:
+            coordinate_axis_vec *= -1
+        new_rot_mat[:, axis] = coordinate_axis_vec
+        axes_aligned[axis] = True
+        new_extent[axis] = extent 
+
+        
+    # check handedness, should theoretically never happen becase reference frame should be right-handed
+    if np.linalg.det(new_rot_mat) < 0:
+        raise ValueError("Rotation matrix has negative determinant")
+        
+    if not axes_aligned[Axis.X] and not axes_aligned[Axis.Y] and not axes_aligned[Axis.Z]:
+        raise ValueError("Could not find consistent alignment")
+    
+    rotated_bb = copy.deepcopy(bb)
+    rotated_bb.R = new_rot_mat
+    rotated_bb.extent = new_extent
+    return rotated_bb

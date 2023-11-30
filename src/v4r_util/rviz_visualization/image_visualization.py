9409be6bc4476b8e94fbe90470e214ceee4652a1
Alexander Haberl <alexander.e360.haberl@tuwien.ac.at>
2023-11-30 14:31:10 +0000

    Implement ros-independant pose estimation visualizer

--- src/v4r_util/rviz_visualization/image_visualization.py
+++ src/v4r_util/rviz_visualization/image_visualization.py
@@ -1,15 +1,11 @@
 import numpy as np
-import rospy
-from sensor_msgs.msg import Image
 import cv2
 import open3d as o3d
-import ros_numpy
-from v4r_util.conversions import ros_poses_to_np_transforms
 import open3d.visualization.rendering as rendering
 
 class PoseEstimationVisualizer():
-    
-    def __init__(self, topic, image_width, image_height, intrinsics_matrix):
+
+    def __init__(self, image_width, image_height, intrinsics_matrix):
         '''
         Renders contours of models and modelnames into an image
         topic: str, topic to publish visualization to
@@ -17,14 +13,13 @@ class PoseEstimationVisualizer():
         image_height: int, height of image
         intrinsics_matrix: flattened camera matrix
         '''
-        self.image_pub = rospy.Publisher(topic, Image, queue_size=10)
         self.image_width = image_width
         self.image_height = image_height
         self.renderer = rendering.OffscreenRenderer(image_width, image_height)
         fx = intrinsics_matrix[0]
-        fy = intrinsics_matrix[5]
+        fy = intrinsics_matrix[4]
         cx = intrinsics_matrix[2]
-        cy = intrinsics_matrix[6]
+        cy = intrinsics_matrix[5]
         pinhole = o3d.camera.PinholeCameraIntrinsic(
             image_width, 
             image_height, 
@@ -33,20 +28,6 @@ class PoseEstimationVisualizer():
             cx, 
             cy)
         self.renderer.setup_camera(pinhole, np.eye(4))
-    
-    def publish_pose_estimation_result(self, ros_image, ros_model_poses, model_meshes, model_names):
-        '''
-        Renders contours of models and modelnames into an image and publishes the result
-        ros_image: sensor_msgs.msg.Image
-        ros_model_poses: list of geometry_msgs.msg.Pose
-        model_meshes: list of open3d.geometry.TriangleMesh, scaled to meters
-        model_name: list of str, names of models
-        '''
-        model_poses = ros_poses_to_np_transforms(ros_model_poses)
-        np_img = ros_numpy.numpify(ros_image)
-        vis_img = self.create_visualization(np_img, model_poses, model_meshes, model_names)
-        vis_img_ros = ros_numpy.msgify(Image, vis_img, encoding='rgb8')
-        self.image_pub.publish(vis_img_ros)
 
     def create_visualization(self, image_scene, model_poses, model_meshes, model_names):
         '''
@@ -56,6 +37,11 @@ class PoseEstimationVisualizer():
         model_meshes: list of open3d.geometry.TriangleMesh, scaled to meters
         model_names: list of str, names of models
         '''
+        # Maybe can move the pinhole construction and setup_camera here to always do it dynamically
+        # instead of checking wheteher the shape of the image is correct
+        assert image_scene.shape[0] == self.image_height
+        assert image_scene.shape[1] == self.image_width
+        
         output_image = image_scene.copy()
         for pose, mesh, name in zip(model_poses, model_meshes, model_names):
             self.renderer.scene.set_background([0.0, 0.0, 0.0, 1.0])  # RGBA
@@ -66,6 +52,7 @@ class PoseEstimationVisualizer():
             mtl = o3d.visualization.rendering.MaterialRecord()
             mtl.base_color = [1.0, 1.0, 1.0, 1.0]  # RGBA, does not replace the mesh color
             mtl.shader = "defaultUnlit"
+        
             self.renderer.scene.add_geometry(name, mesh, mtl)
 
             image_model = self.renderer.render_to_image()
@@ -88,3 +75,5 @@ class PoseEstimationVisualizer():
                 1, 
                 cv2.LINE_AA)
         return output_image
+    
+
